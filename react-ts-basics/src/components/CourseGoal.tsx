451698bca4cfd39d4709eb07ea621183668717ad
a1x40v <a1x40v@gmail.com>
2024-07-22 16:38:01 +0300

    Start to code basic react app

---
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,36 @@
+import { type FC, type PropsWithChildren } from 'react';
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{
+  title: string;
+}>;
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
