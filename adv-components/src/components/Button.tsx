cfa2d59655fdc598b5865914c374901bd20b5be0
a1x40v <a1x40v@gmail.com>
2024-07-28 10:47:14 +0300

    Create new project. Build wrapper Button component

---
+++ adv-components/src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { type ComponentPropsWithRef } from 'react';
+
+type ButtonProps = ComponentPropsWithRef<'button'> & {
+  href?: never;
+};
+
+type AnchorProps = ComponentPropsWithRef<'a'> & {
+  href?: string;
+};
+
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return 'href' in props;
+}
+
+export default function Button(props: ButtonProps | AnchorProps) {
+  if (isAnchorProps(props)) {
+    return <a href={props.href} className="button" {...props}></a>;
+  }
+
+  return (
+    <button className="button" {...props}>
+      Button
+    </button>
+  );
+}
